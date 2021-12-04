b81b663a12ff3f524fcff53bc00f7b0bb96d7808
ktaehyun <k_taehyun94@naver.com>
2021-12-04 09:15:32 +0900

    Add 211204 code file

---
+++ target_number.py
@@ -0,0 +1,13 @@
+def solution(numbers, target):
+    
+    q = [-numbers[0], numbers[0]]
+    for number in numbers[1:]:
+        temp = []
+        while q:
+            pop_num = q.pop()
+            temp.append(pop_num - number)
+            temp.append(pop_num + number)
+        q = temp
+        print('q:', q, '\n')
+        
+    return q.count(target)
