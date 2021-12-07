0990b4a09a36fde1e4fb01e550737db5b3dc5b6c
ktaehyun <k_taehyun94@naver.com>
2021-12-07 23:03:43 +0900

    Add 211207 code file

---
+++ 369game.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for x in range(1, n+1):
+    str_x = str(x)
+    sum_count = str_x.count('3') + str_x.count('6') + str_x.count('9')
+    if sum_count >= 1:
+        print('-' * sum_count, end=' ')
+        continue
+    else:
+        print(x, end=' ')
