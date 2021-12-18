7025a6be554860538c3a69a741fc5070049d20f4
ktaehyun <k_taehyun94@naver.com>
2021-12-18 10:29:04 +0900

    Add 211202 code file

--- carpet.py
+++ carpet.py
@@ -1,9 +1,7 @@
 def solution(brown, yellow):
-    
     for a in range(3, brown):
         for b in range(3, a+1):
             if brown==2*(a+b-2) and yellow==(a*b)-brown:
                 result = [a, b]
                 break
-        
     return result
