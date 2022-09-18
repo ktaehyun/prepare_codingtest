9ac4566e8231aded23a347bcca6f7847061cfec3
ktaehyun <86669008+ktaehyun@users.noreply.github.com>
2022-09-18 10:59:02 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,34 @@
+from collections import deque
+    
+def checking(target, words, q, length, moment, time):
+    time += 1
+    count, temp = 0, []
+    while q:
+        now = q.popleft()
+        if now == target:
+            return time
+        for word in words:
+            for i in range(length):
+                if word not in moment and now[i]==word[i]:
+                    count += 1
+            if count == length - 1:
+                temp.append(word)
+                moment.append(word)
+            else:
+                count = 0
+    return checking(target, words, deque(temp), length, moment, time)
+
+def solution(begin, target, words):
+    filter_words = list(filter(lambda x: target in x, words))
+    if len(filter_words) == 0:
+        return 0
+    q, count, length = deque(), 0, len(begin)
+    for word in words:
+        for i in range(length):
+            if begin[i] == word[i]:
+                count += 1
+        if count == length - 1:
+            q.append(word)
+        else:
+            count = 0
+    return checking(target, words, q, length, list(q), 0)
