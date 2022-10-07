f8b5a81cc45f8d749f7a3bf8d7b0fca0ae24d246
ktaehyun <86669008+ktaehyun@users.noreply.github.com>
2022-10-07 20:55:28 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -1,34 +1,23 @@
 from math import ceil
+from collections import deque
 
 def solution(progresses, speeds):
     
-    days = [ceil((100-progress)/speed) for progress, speed in zip(progresses, speeds)]
+    result = []
     
-    result, now_day, count = [], days[0], 1
-    for i in range(1, len(days)):
-        if days[i] > now_day:
-            now_day = days[i]
+    queue = deque([ceil((100-progress)/speed)
+                        for progress,speed in zip(progresses,speeds)])
+    
+    prev = queue.popleft()
+    count = 1
+    while queue:
+        now = queue.popleft()
+        if prev < now:
             result.append(count)
             count = 1
+            prev = now
         else:
             count += 1
     result.append(count)
     
     return result
-    
-
-# from math import ceil
-
-# def solution(progresses, speeds):
-#     answer = [0]
-    
-#     pg = ceil((100-progresses[0])/speeds[0])
-    
-#     for p, s in zip(progresses, speeds):
-#         if pg >= ceil((100-p)/s):
-#             answer.append(answer.pop()+1)
-#         if pg < ceil((100-p)/s):
-#             answer.append(1)
-#             pg = ceil((100-p)/s)
-            
-#     return answer
