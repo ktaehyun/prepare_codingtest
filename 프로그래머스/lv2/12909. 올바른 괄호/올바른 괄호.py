c1a43a79fce8eda3a7251154b8f448e1ff23c69d
ktaehyun <86669008+ktaehyun@users.noreply.github.com>
2022-10-07 20:45:27 +0900

    [level 2] Title: 올바른 괄호, Time: 11.14 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -3,15 +3,17 @@ from collections import deque
 def solution(s):
     
     answer = True
-    check = 0
-    que = deque(s)
     
-    while(que):
-        gawlho = que.popleft()
-        if gawlho == "(" :
-            check+=1
-        else :
-            check-=1
+    queue = deque(s)
+    check = 0    
+    while queue:
+        gwalho = queue.popleft()
+        
+        if gwalho == "(" :
+            check += 1
+        else:
+            check -= 1
+            
         if check < 0 :
             answer = False
             break
