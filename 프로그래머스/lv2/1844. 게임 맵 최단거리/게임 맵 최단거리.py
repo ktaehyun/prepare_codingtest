229f3bc5434912715a94472349978c9a18e52a6c
ktaehyun <86669008+ktaehyun@users.noreply.github.com>
2022-10-07 21:32:40 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.98 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,30 +1,59 @@
 from collections import deque
 
 def solution(maps):
-    n = len(maps[0])
-    m = len(maps)
+    
+    result = []
+    
+    n, m = len(maps[0]), len(maps)
     visited = [[False for col in range(n)] for row in range(m)]
     visited[0][0] = True
-
-    queue = deque()
-    queue.append((0,0,1))
-
+    
+    queue = deque([(0,0,1)])
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    
     while queue:
-        yIndex, xIndex, distance = queue.popleft()
-        if xIndex == n-1 and yIndex == m-1:
-            return distance
-
-        if 0<=xIndex+1<n and visited[yIndex][xIndex+1] == False and maps[yIndex][xIndex+1] == 1:
-            queue.append((yIndex, xIndex+1, distance+1))
-            visited[yIndex][xIndex+1] = True
-        if 0<=yIndex+1<m and visited[yIndex+1][xIndex] == False and maps[yIndex+1][xIndex] == 1:
-            queue.append((yIndex+1, xIndex, distance+1))
-            visited[yIndex+1][xIndex] = True
-        if 0<=xIndex-1<n and visited[yIndex][xIndex-1] == False and maps[yIndex][xIndex-1] == 1:
-            queue.append((yIndex, xIndex-1, distance+1))
-            visited[yIndex][xIndex-1] = True
-        if 0<=yIndex-1<m and visited[yIndex-1][xIndex] == False and maps[yIndex-1][xIndex] == 1:
-            queue.append((yIndex-1, xIndex, distance+1))
-            visited[yIndex-1][xIndex] = True
-
+        x, y, distance = queue.popleft()
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0<=nx<m and 0<=ny<n and visited[nx][ny]==False and maps[nx][ny]==1:
+                queue.append((nx, ny, distance+1))
+                visited[nx][ny] = True
+            if nx==m-1 and ny==n-1:
+                result.append(distance+1)
+    
+    if result:
+        return min(result)
     return -1
+
+# from collections import deque
+
+# def solution(maps):
+#     n = len(maps[0])
+#     m = len(maps)
+#     visited = [[False for col in range(n)] for row in range(m)]
+#     visited[0][0] = True
+
+#     queue = deque()
+#     queue.append((0,0,1))
+
+#     while queue:
+#         yIndex, xIndex, distance = queue.popleft()
+#         if xIndex == n-1 and yIndex == m-1:
+#             return distance
+
+#         if 0<=xIndex+1<n and visited[yIndex][xIndex+1] == False and maps[yIndex][xIndex+1] == 1:
+#             queue.append((yIndex, xIndex+1, distance+1))
+#             visited[yIndex][xIndex+1] = True
+#         if 0<=yIndex+1<m and visited[yIndex+1][xIndex] == False and maps[yIndex+1][xIndex] == 1:
+#             queue.append((yIndex+1, xIndex, distance+1))
+#             visited[yIndex+1][xIndex] = True
+#         if 0<=xIndex-1<n and visited[yIndex][xIndex-1] == False and maps[yIndex][xIndex-1] == 1:
+#             queue.append((yIndex, xIndex-1, distance+1))
+#             visited[yIndex][xIndex-1] = True
+#         if 0<=yIndex-1<m and visited[yIndex-1][xIndex] == False and maps[yIndex-1][xIndex] == 1:
+#             queue.append((yIndex-1, xIndex, distance+1))
+#             visited[yIndex-1][xIndex] = True
+
+#     return -1
