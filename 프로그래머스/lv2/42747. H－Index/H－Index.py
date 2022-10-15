6ee69cdf168619704eff4db279f6fd588cb88e23
ktaehyun <86669008+ktaehyun@users.noreply.github.com>
2022-10-15 10:48:33 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,13 @@
+def solution(citations):
+    
+    h = 0
+    
+    for _ in citations:
+        h_index = [citation for citation in citations if citation >= h]
+        if len(h_index) >= h:
+            h += 1
+        if len(h_index) < h:
+            h -= 1
+            break
+            
+    return h
