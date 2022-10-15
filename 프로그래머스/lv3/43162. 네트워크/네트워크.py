e9de3f60cf23e684bf011bb30841aa9018db63c5
ktaehyun <86669008+ktaehyun@users.noreply.github.com>
2022-10-15 10:53:23 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,4 +1,5 @@
 def connection(n, computers, cpt, index, network, count):
+    
     while cpt:
         now = cpt.pop(0)
         for i in range(n):
@@ -13,27 +14,5 @@ def connection(n, computers, cpt, index, network, count):
         return count
 
 def solution(n, computers):
+    
     return connection(n, computers, [computers[0]], 0, [0] * n, 1)
-
-
-
-# def ntw(id, cnt, n, network, computers):
-#     q = [(computers[id], id)]
-#     cnt += 1
-#     while q:
-#         cpt, index = q.pop(0)
-#         for i in range(n):
-#             if cpt[i] == 1:
-#                 if i != index and network[i] == 0:
-#                     q.append((computers[i], i))
-#                 if network[i] == 0:
-#                     network[i] = cnt
-#     if network.count(0) == 0:
-#         return cnt
-#     else:
-#         return ntw(network.index(0), cnt, n, network, computers)
-
-# def solution(n, computers):
-#     network = [0] * n            
-#     cnt = 0
-#     return ntw(0, cnt, n, network, computers)
