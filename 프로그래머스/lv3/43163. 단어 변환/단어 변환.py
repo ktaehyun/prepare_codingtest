c721c4df125c360236cb618dbbaff284ef5ad620
TaeHyun Kim <k_taehyun94@naver.com>
2023-02-09 12:46:41 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.py
@@ -1,5 +1,21 @@
 from collections import deque
     
+def comparison(now, words, length, moment, count, temp):
+    for word in words:
+        for i in range(length):
+            if word in moment:
+                break
+            elif now[i] == word[i]:
+                count += 1
+        if count == length - 1:
+            temp.append(word)
+            moment.append(word)
+        else:
+            count = 0
+            
+    return temp, moment
+        
+    
 def checking(target, words, q, length, moment, time):
     time += 1
     count, temp = 0, []
@@ -7,30 +23,16 @@ def checking(target, words, q, length, moment, time):
         now = q.popleft()
         if now == target:
             return time
-        for word in words:
-            for i in range(length):
-                if word in moment:
-                    break
-                elif now[i]==word[i]:
-                    count += 1
-            if count == length - 1:
-                temp.append(word)
-                moment.append(word)
-            else:
-                count = 0
+        temp, moment = comparison(now, words, length, moment, count, temp)
+        
     return checking(target, words, deque(temp), length, moment, time)
+
 
 def solution(begin, target, words):
     filter_words = list(filter(lambda x: target in x, words))
     if len(filter_words) == 0:
         return 0
     q, count, length = deque(), 0, len(begin)
-    for word in words:
-        for i in range(length):
-            if begin[i] == word[i]:
-                count += 1
-        if count == length - 1:
-            q.append(word)
-        else:
-            count = 0
+    q, _ = comparison(begin, words, length, [], count, q)
+    
     return checking(target, words, q, length, list(q), 0)
