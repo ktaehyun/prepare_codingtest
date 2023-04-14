9eba548ff27adb9eca81325d0d75ea04cba898c1
TaeHyun Kim <k_taehyun94@naver.com>
2023-04-14 21:55:39 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.py
@@ -0,0 +1,26 @@
+def solution(tickets):
+    n = len(tickets)
+    answer = []
+    
+    def dfs():
+        if len(stack) == n+1 :
+            a = [stack[i] for i in range(n+1)]
+            answer.append(a)
+        for i in range(n):
+            if not visited[i] and stack[-1] == tickets[i][0] :
+                visited[i] = 1
+                stack.append(tickets[i][1])
+                dfs()
+                visited[i] = 0
+                stack.pop()
+
+    for i in range(n):
+        visited = [0] * n
+        stack = []
+        if tickets[i][0] == "ICN":
+            visited[i] = 1
+            stack.append(tickets[i][0])
+            stack.append(tickets[i][1])
+            dfs()
+
+    return min(answer)
