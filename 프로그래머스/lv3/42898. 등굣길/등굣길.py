af53aca804b14b5ece85ea791c69cd41723e8ac6
TaeHyun Kim <k_taehyun94@naver.com>
2023-09-07 00:29:47 +0900

    [level 3] Title: 등굣길, Time: 1.81 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -3,7 +3,7 @@ def solution(m, n, puddles):
     puddles = [[q-1,p-1] for [p,q] in puddles]  # 미리 puddles 좌표 거꾸로
     dp = [[0] * m for _ in range(n)]  # dp 초기화
     dp[0][0] = 1  # 시작 위치
-
+    
     for i in range(n):
         for j in range(m):
             if i==0 and j==0:
@@ -11,6 +11,9 @@ def solution(m, n, puddles):
             if [i, j] in puddles:  # 웅덩이 위치의 경우 0
                 dp[i][j] = 0
             else:  # 현재 칸은 왼쪽 칸, 위 칸의 합산
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000007
+                # print('\n', dp)
+                # print("i-1, j : ", i-1, j)
+                # print("i, j-1 : ", i, j-1)
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1])
                 
-    return dp[n-1][m-1]
+    return dp[n-1][m-1] % 1000000007
