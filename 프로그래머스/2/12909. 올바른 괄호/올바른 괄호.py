31670606d9d71b99d08ecb5d1289c03422fa7387
TaeHyun Kim <k_taehyun94@naver.com>
2023-10-21 15:11:08 +0900

    [level 2] Title: 올바른 괄호, Time: 6.61 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(s):
+    
+    cnt = 0
+    for now in s:
+        if now == "(":
+            cnt += 1
+        else:
+            cnt -= 1
+        if cnt < 0:
+            return False
+    if cnt == 0:
+        return True
+    return False
