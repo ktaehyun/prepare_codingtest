6085b44870a5e5e335ce5649efba54a6e3eeb85c
TaeHyun Kim <k_taehyun94@naver.com>
2024-10-13 20:28:01 +0900

    [level 1] Title: 실패율, Time: 3316.86 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -0,0 +1,19 @@
+def solution(N, stages):
+    
+    answer = []
+    
+    stages.sort()
+    
+    result = []
+    for n in range(1, N+1):
+        if n in stages:
+            fail_rate = stages.count(n) / len(stages[stages.index(n):])
+            result.append((fail_rate, n))
+        else:
+            result.append((0, n))
+            
+    result.sort(key = lambda x: (-x[0], x[1]))
+    for rs in result:
+        answer.append(rs[1])
+        
+    return answer
