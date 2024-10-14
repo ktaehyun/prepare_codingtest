c785b4d40bb4ab80d8af8ae7f264c7afb04442c4
TaeHyun Kim <k_taehyun94@naver.com>
2024-10-15 00:15:58 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 15.54 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,28 +1,39 @@
 from collections import deque
 
 def solution(maps):
+    answer = 0
+
+    # 상하좌우
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+
+    def bfs(x, y):
+        queue = deque()
+        queue.append((x, y))
+
+        # queue가 빌 때까지 반복
+        while queue:
+            x, y = queue.popleft()
+
+            # 상하좌우 칸 확인하기
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+
+                # 맵을 벗어나면 무시하기
+                if nx < 0 or nx >= len(maps) or ny < 0 or ny >= len(maps[0]): continue
+
+                # 벽이면 무시하기
+                if maps[nx][ny] == 0:  continue
+
+                # 처음 지나가는 길이면 거리계산하고 다시 상하좌우 확인하기
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[x][y] + 1
+                    queue.append((nx, ny))    # 재귀
+
+        # 상대 팀 진영(제일 오른쪽 아래 칸)까지의 거리 반환
+        return maps[len(maps)-1][len(maps[0])-1]
+
+    answer = bfs(0, 0)
     
-    result = []
-    
-    n, m = len(maps[0]), len(maps)
-    visited = [[False for col in range(n)] for row in range(m)]
-    visited[0][0] = True
-    
-    queue = deque([(0,0,1)])
-    dx = [0,0,1,-1]
-    dy = [1,-1,0,0]
-    
-    while queue:
-        x, y, distance = queue.popleft()
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if 0<=nx<m and 0<=ny<n and visited[nx][ny]==False and maps[nx][ny]==1:
-                queue.append((nx, ny, distance+1))
-                visited[nx][ny] = True
-            if nx==m-1 and ny==n-1:
-                result.append(distance+1)
-    
-    if result:
-        return min(result)
-    return -1
+    return -1 if answer==1 else answer    # 상대 팀 진영에 도착할 수 없을 때 -1
