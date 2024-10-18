640731ea79744aafe64c66b95b57fa27fb62932b
TaeHyun Kim <k_taehyun94@naver.com>
2024-10-18 23:01:45 +0900

    [level 2] Title: 가장 큰 수, Time: 99.69 ms, Memory: 27.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,10 +1,8 @@
 def solution(numbers):
-    
     str_numbers = [str(num) for num in numbers]
     sort_numbers = sorted(str_numbers, key=lambda x: (x*4)[:4], reverse=True)
     
     if sort_numbers[0] != '0':
-        result = ''.join(sort_numbers)
-        return result
-    else:
-        return '0'
+        ans = ''.join(sort_numbers)
+        return ans
+    return '0'
