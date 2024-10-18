11c35a3d6365f380e3aa3449cbd1c0c533b3aa1b
TaeHyun Kim <k_taehyun94@naver.com>
2024-10-19 00:59:51 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 20.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -5,28 +5,31 @@ dy = [1, -1, 0, 0]
 
 def bfs(board, dir):
     n = len(board)
-    price = [[1e9]*n for _ in range(n)]
+    price = [[int(1e9)] * n for _ in range(n)]
     price[0][0] = 0
-    q = deque([(0,0,0,dir)])
-    while q:
-        x, y, c, direc = q.popleft()
+    queue = deque([(0, 0, 0, dir)])  # (시작X, 시작Y, 시작Cost, 시작Direction)
+    while queue:
+        x, y, c, direction = queue.popleft()
         if x==n-1 and y==n-1:
             continue
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
-            if nx<0 or nx>=n or ny<0 or ny>=n or board[nx][ny]==1:
+
+            if nx<0 or nx>=n or ny<0 or ny>=n or board[nx][ny] == 1:
                 continue
-            if d == direc:
+
+            if d == direction:
                 nc = c + 100
             else:
                 nc = c + 600
-                
+
             if nc < price[nx][ny]:
                 price[nx][ny] = nc
-                q.append((nx, ny, nc, d))
+                queue.append((nx, ny, nc, d))
                 
     return price[-1][-1]
+
 
 def solution(board):
     answer = min(bfs(board,0), bfs(board,2))
