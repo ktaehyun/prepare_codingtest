fd1d5e476ff7ab4cebea81016da76ea8fa513124
TaeHyun Kim <k_taehyun94@naver.com>
2024-10-19 00:56:57 +0900

    [level 3] Title: 합승 택시 요금, Time: 2505.38 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.py
@@ -1,21 +1,19 @@
 def solution(n, s, a, b, fares):
-    ans = 200000001
-    cost = [[20000001] * (n+1) for _ in range(n+1)]
+    ans = 1e9
+    cost = [[1e9]*(n+1) for _ in range(n+1)]
     
-    def floyd_warshall():
-        for k in range(1, n+1):
-            for i in range(1, n+1):
-                for j in range(1, n+1):
-                    if i == j:
-                        cost[i][j] = 0
-                    else:
-                        cost[i][j] = min(cost[i][k] + cost[k][j], cost[i][j])
-    
-    for i, j, c in fares:
+    for i,j,c in fares:
         cost[i][j] = c
         cost[j][i] = c
-    floyd_warshall()
         
+    for k in range(1, n+1):
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if i == j:
+                    cost[i][j] = 0
+                else:
+                    cost[i][j] = min(cost[i][j], cost[i][k]+cost[j][k])
+                        
     for i in range(1, n+1):
-        ans = min(cost[s][i] + cost[i][a] + cost[i][b], ans)
+        ans = min(cost[s][i]+cost[i][a]+cost[i][b], ans)
     return ans
