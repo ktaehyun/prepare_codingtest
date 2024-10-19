0614fb0e4b5992ee2cf486c41caa9d7ab1674b7d
TaeHyun Kim <k_taehyun94@naver.com>
2024-10-19 12:06:41 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 2384.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -2,8 +2,9 @@ discounts = [10, 20, 30, 40]
 ans = [-1, -1]
 
 def solution(users, emoticons):
+    
     n, m = len(users), len(emoticons)
-    discount_list = [0]*m
+    discount_list = [0] * m
     
     def search(idx):
         global ans
@@ -21,7 +22,7 @@ def solution(users, emoticons):
             if sale>ans[0] or (sale==ans[0] and cost>ans[1]):
                 ans = [sale, cost]
             return
-                    
+        
         for i in range(4):
             discount_list[idx] = discounts[i]
             search(idx+1)
